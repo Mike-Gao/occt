a151681f49e3f365a76c0491bec127c72b594258
nds <nds@opencascade.com>
2019-02-07 11:59:53 +0300

    0030484: Visualization - 2d persistent: order of detection doesn't coincide with order of objects creation

--- src/SelectMgr/SelectMgr_ViewerSelector.cxx
+++ src/SelectMgr/SelectMgr_ViewerSelector.cxx
@@ -276,6 +276,9 @@ void SelectMgr_ViewerSelector::checkOverlap (const Handle(SelectBasics_Sensitive
   aCriterion.Priority  = anOwner->Priority();
   aCriterion.Depth     = aPickResult.Depth();
   aCriterion.MinDist   = aPickResult.DistToGeomCenter();
+  if (!aSelectable.IsNull() && !aSelectable->TransformPersistence().IsNull() &&
+     aSelectable->TransformPersistence()->Mode() == Graphic3d_TMF_2d)
+    aCriterion.MinDist = 0; // having 2D persistent, 3D geometry point is not required
   aCriterion.ToPreferClosest = preferclosest;
 
   if (SelectMgr_SortCriterion* aPrevCriterion = mystored.ChangeSeek (anOwner))
