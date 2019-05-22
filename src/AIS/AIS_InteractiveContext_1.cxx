7a394be7c3a61b2d60d1e62159754f2947fba6d5
nds <nds@opencascade.com>
2019-05-22 11:37:03 +0300

    0030725: Visualization - remove Display calling from SetSelected methods of context
    (cherry picked from commit 3356a2f1fcde55717e3e8a4a59059ee0accb6ba7)
    (cherry picked from commit 86fb569627e1d89f8cfd991bd4dcb39235fad9f6)
    (cherry picked from commit 96923ab6908271ca033efda12ca529dd9b1aa0a0)

--- src/AIS/AIS_InteractiveContext_1.cxx
+++ src/AIS/AIS_InteractiveContext_1.cxx
@@ -818,7 +818,7 @@ void AIS_InteractiveContext::SetSelected (const Handle(AIS_InteractiveObject)& t
 
   if (!myObjects.IsBound (theObject))
   {
-    Display (theObject, Standard_False);
+    return;
   }
 
   Handle(SelectMgr_EntityOwner) anOwner = theObject->GlobalSelOwner();
@@ -910,7 +910,7 @@ void AIS_InteractiveContext::SetSelected (const Handle(SelectMgr_EntityOwner)& t
   }
 
   if (!myObjects.IsBound (anObject))
-    Display (anObject, Standard_False);
+    return;
 
   if (myAutoHilight)
   {
