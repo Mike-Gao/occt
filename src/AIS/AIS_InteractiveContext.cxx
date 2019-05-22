f25bebd7b387ab345c65dfc03a01f601555ad6a3
nds <nds@opencascade.com>
2019-05-22 17:33:02 +0300

    0030728: Visualization - using one implementation of highlight/unhighlight in context
    (cherry picked from commit 36b87936acf5a89d573663b5e8d086a4a4a084d3)
    
    # Conflicts:
    #	src/AIS/AIS_InteractiveContext_1.cxx
    (cherry picked from commit 7fc133f086611b4f4e36b658f99c29012da921d1)
    
    # Conflicts:
    #	src/AIS/AIS_InteractiveContext_1.cxx
    (cherry picked from commit a06b8e8308efef642cc5c2c5ce6261aae276d530)
    (cherry picked from commit 0d5a72ab1638e4ed3d3eb72b15430ee7337f69bc)
    
    # Conflicts:
    #	src/AIS/AIS_InteractiveContext.cxx
    (cherry picked from commit 9e9c9c84bdc5fa579105190b12ec82fbf8db3081)

--- src/AIS/AIS_InteractiveContext.cxx
+++ src/AIS/AIS_InteractiveContext.cxx
@@ -2000,10 +2000,10 @@ void AIS_InteractiveContext::EraseGlobal (const Handle(AIS_InteractiveObject)& t
 }
 
 //=======================================================================
-//function : unhighlightOwners
+//function : unselectOwners
 //purpose  :
 //=======================================================================
-void AIS_InteractiveContext::unhighlightOwners (const Handle(AIS_InteractiveObject)& theObject)
+void AIS_InteractiveContext::unselectOwners (const Handle(AIS_InteractiveObject)& theObject)
 {
   SelectMgr_SequenceOfOwner aSeq;
   for (AIS_NListOfEntityOwner::Iterator aSelIter (mySelection->Objects()); aSelIter.More(); aSelIter.Next())
@@ -2037,7 +2037,7 @@ void AIS_InteractiveContext::ClearGlobal (const Handle(AIS_InteractiveObject)& t
     return;
   }
 
-  unhighlightOwners (theIObj);
+  unselectOwners (theIObj);
 
   myMainPM->Erase (theIObj, -1);
   theIObj->ErasePresentations (true); // make sure highlighting presentations are properly erased
