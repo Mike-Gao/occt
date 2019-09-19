f290de8a42aae6249892c7f63c2457a24fb88d9f
nds <nds@opencascade.com>
2019-09-19 15:13:42 +0300

    0030949: Foundation Classes - Dump improvement for OCCT classes
    1. new file Standard_Dump to prepare and parse Dump in JSON format for OCCT objects
    2. some presentations cover the proposed dump functionality.
    3. 'bounding', 'vaspects' has '-dumpJson' field to see the DumpJson result
    4. Bnd_Box constructor with min/max points is implemented to use Dump of this class in Dump BVH_Box
    5. Limitation (some classes of Graphic3d, Prs3d has not full filling for DumpJson)
    
    (cherry picked from commit 0904aa6395c6e00d07ffa83ae21334236b0eb51b)
    
    # Conflicts:
    #	src/OpenGl/OpenGl_Group.hxx
    (cherry picked from commit 2e6c171af255cee040bd46af364094dc49ca2148)

--- src/OpenGl/OpenGl_Group.hxx
+++ src/OpenGl/OpenGl_Group.hxx
@@ -111,6 +111,9 @@ public:
   //! Returns OpenGL capping filling aspect.
   const OpenGl_CappingPlaneResource* AspectFillCapping() const { return myAspectFillCapping; }
 
+  //! Dumps the content of me into the stream
+  Standard_EXPORT virtual void DumpJson (Standard_OStream& theOStream, const Standard_Integer theDepth = -1) const Standard_OVERRIDE;
+  
 protected:
 
   Standard_EXPORT virtual ~OpenGl_Group();
