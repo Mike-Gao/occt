c6f09dddb2b55bb2481da9305ff19a7cd2d5a9a4
nds <nds@opencascade.com>
2020-01-17 19:02:31 +0300

    0030268: Inspectors - improvements in VInspector plugin
    # do not use virtual methods in classes where there are no virtual destructor

--- src/gp/gp_Ax1.hxx
+++ src/gp/gp_Ax1.hxx
@@ -206,7 +206,7 @@ public:
 
 
   //! Dumps the content of me into the stream
-  Standard_EXPORT virtual void DumpJson (Standard_OStream& theOStream, const Standard_Integer theDepth = -1) const;
+  Standard_EXPORT void DumpJson (Standard_OStream& theOStream, const Standard_Integer theDepth = -1) const;
 
   //! Inits the content of me into the stream
   Standard_EXPORT Standard_Boolean InitJson (const Standard_SStream& theSStream, Standard_Integer& theStreamPos);
