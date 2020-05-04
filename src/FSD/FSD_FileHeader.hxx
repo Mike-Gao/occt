ffe942a5266bd442db2c8f106cfb86e304d1f56f
abv <abv@opencascade.com>
2020-05-04 22:25:03 +0300

    0031546: Application Framework - Memory leak (100 bytes) on Load / Close OCAF document
    Class Standard_BaseDriver is inherited from Standard_Transient, its descendants are updated accordingly.
    Handle is used to manipulate objects of this class and its descendants (instead of references or raw pointers) to ensure automatic destruction.
    
    Added test bugs caf bug31546
    
    Related:
    - Standard_OVERRIDE is added in declarations of virtual methods in descendants of Storage_BaseDriver
    - Methods operator << and operator >> are removed in descendants of Storage_BaseDriver (they repeat the same methods inherited from the base class)
    - Typedef PCDM_BaseDriverPointer is marked as deprecated
    - Unused class DDI_Ostream is removed
    - Private field Standard_Transient::count is renamed to myRefCount_ to avoid compiler warnings if the same name is used within the scope of any descendant class
    - Reporting specific error messages is restored in DRAW commands fsdread and fsdwrite

--- src/FSD/FSD_FileHeader.hxx
+++ src/FSD/FSD_FileHeader.hxx
@@ -13,6 +13,8 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
+#include <Standard_PrimitiveTypes.hxx>
+
 #ifndef _FSD_FileHeader_HeaderFile
 #define _FSD_FileHeader_HeaderFile
 
