af9e0ec6ee4a07816d398086d83ecf0d2947cdf0
asuraven <andrey.suravenkov@opencascade.com>
2020-06-10 10:59:07 +0300

    Add files

---
+++ samples/standard/samples_src/BaseSample.h
@@ -0,0 +1,57 @@
+
+
+#ifndef BASESAMPLE_H
+#define BASESAMPLE_H
+
+#include <sstream>
+
+#include <Standard_Transient.hxx>
+#include <NCollection_Vector.hxx>
+#include <AIS_InteractiveObject.hxx>
+#include <TCollection_AsciiString.hxx>
+#include <Standard_Handle.hxx>
+
+class BaseSample;
+DEFINE_STANDARD_HANDLE(BaseSample, Standard_Transient)
+
+class BaseSample: public Standard_Transient
+{
+public:
+	Standard_Boolean                                  IsProcessed();
+  NCollection_Vector<Handle(AIS_InteractiveObject)> Get2dObject();
+  NCollection_Vector<Handle(AIS_InteractiveObject)> Get3dObject();
+  TCollection_AsciiString                           GetResult();
+  TCollection_AsciiString                           GetCode();
+
+  void Process(TCollection_AsciiString theSampleName);
+  void SetCodePach(TCollection_AsciiString theSampleSourcePach);
+
+  DEFINE_STANDARD_RTTI_INLINE(BaseSample, Standard_Transient)
+
+protected:
+  virtual void ExecuteSample(TCollection_AsciiString theSampleName) = 0;
+          void FindSourceCode(TCollection_AsciiString theSampleName);
+
+private:
+  void             TraceError(TCollection_AsciiString theErrorMessage);
+
+  Standard_Integer FindEndOfPhrase(TCollection_AsciiString theText, 
+                                   TCollection_AsciiString theRegexpTemplate);
+  Standard_Integer FindClosingBracket(TCollection_AsciiString theText,
+                                      Standard_Integer theOpeningBracketIndex,
+                                      Standard_Character theClosingBracketSymbol);
+
+protected:
+  Standard_Boolean                                  myIsProcessed;
+	NCollection_Vector<Handle(AIS_InteractiveObject)> myObject2d;
+  NCollection_Vector<Handle(AIS_InteractiveObject)> myObject3d;
+
+  // Standard_OStream* strem = dynamic_cast<Standard_OStream*>(&myResult);
+  std::ostringstream                                myResult;
+  TCollection_AsciiString                           myCode;
+	TCollection_AsciiString                           myCodePath;
+
+  static const TCollection_AsciiString FILE_EXTENSION;
+};
+
+#endif  //BASESAMPLE_H
---
+++ samples/standard/samples_src/GeometrySamples.h
@@ -0,0 +1,78 @@
+#ifndef GEOMETRYSAMPLES_H
+#define GEOMETRYSAMPLES_H
+
+
+#include "BaseSample.h"
+
+
+class GeometrySamples;
+DEFINE_STANDARD_HANDLE(GeometrySamples, BaseSample)
+
+class GeometrySamples : public BaseSample
+{
+public:
+  DEFINE_STANDARD_RTTI_INLINE(GeometrySamples, BaseSample)
+
+  virtual ~GeometrySamples() {}
+protected:
+  void ExecuteSample(TCollection_AsciiString theSampleName) override;
+
+private:
+  // One function for every sample
+  void ZeroDimensionObjects3dSample();
+  void Vectors3dSample();
+  void InfinityLines3dSample();
+  void SecondOrderCurves3dSample();
+  void PlaneSurfaces3dSample();
+  void SecondOrderSurfaces3dSample();
+  void ZeroDimensionObjects2dSample();
+  void Vectors2dSample();
+  void InfinityLines2dSample();
+  void SecondOrderCurves2dSample();
+  void BarycenterPoint3dSample();
+  void RotatedVector3dSample();
+  void MirroredLine3dSample();
+  void ScaledEllipse3dSample();
+  void TransformedCylinder3dSample();
+  void TranslatedTorus3dSample();
+  void ConjugateObjects3dSample();
+  void TangentAndNormal3dSample();
+  void ProjectionOfPoint3dSample();
+  void MinimalDistance3dSample();
+  void Intersection3dSample();
+  void TranslatedPoint2dSample();
+  void RotatedDirection2dSample();
+  void MirroredAxis2dSample();
+  void TransformedEllipse2dSample();
+  void TangentAndNormal2dSample();
+  void ProjectionOfPoint2dSample();
+  void MinimalDistance2dSample();
+  void Intersection2dSample();
+  void PointInfo3dSample();
+  void EllipseInfo3dSample();
+  void PointInfo2dSample();
+  void CircleInfo2dSample();
+  void ParamSecondOrderCurves3dSample();
+  void FreeStyleCurves3dSample();
+  void AnalyticalSurfaces3dSample();
+  void FreeStyleSurfaces3dSample();
+  void ParamSecondOrderCurves2dSample();
+  void FreeStyleCurves2dSample();
+  void TrimmedCurve3dSample();
+  void OffsetCurve3dSample();
+  void BSplineFromCircle3dSample();
+  void TrimmedSurface3dSample();
+  void OffsetSurface3dSample();
+  void ExtrusionSurface3dSample();
+  void RevolutionSurface3dSample();
+  void TrimmedCurve2dSample();
+  void ParamOffsetCurve3dSample();
+  void BoundingBoxOfSurface3dSample();
+  void BoundingBoxOfCurves3dSample();
+  void BoundingBoxOfCurves2dSample();
+  void JsonWithCircleRadiusSample();
+  void JsonWithBSplineCurveInfoSample();
+
+};
+
+#endif  //GEOMETRYSAMPLES_H
