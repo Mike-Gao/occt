1b2bce84ea10b2860c3c4f3916353f9f3e12bfc1
asuraven <andrey.suravenkov@opencascade.com>
2020-08-07 14:27:18 +0300

    Copyright & selection mode

--- samples/standard/qt/Tutorial/src/View.h
+++ samples/standard/qt/Tutorial/src/View.h
@@ -1,3 +1,16 @@
+// Copyright (c) 2020 OPEN CASCADE SAS
+//
+// This file is part of Open CASCADE Technology software library.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License version 2.1 as published
+// by the Free Software Foundation, with special exception defined in the file
+// OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
+// distribution for complete text of the license and disclaimer of any warranty.
+//
+// Alternatively, this file may be used under the terms of Open CASCADE
+// commercial license or contractual agreement.
+
 #ifndef VIEW_H
 #define VIEW_H
 
@@ -17,7 +30,7 @@ class TopoDS_Shape;
 class QRubberBand;
 
 enum class CurrentAction3d { Nothing, DynamicZooming, WindowZooming, 
-                             DynamicPanning, GlobalPanning, DynamicRotation };
+                             DynamicPanning, GlobalPanning, DynamicRotation, ObjectDececting };
 enum class ViewAction { FitAll, FitArea, Zoom, Pan, GlobalPan, Front, Back, Top, Bottom, 
                         Left, Right, Axo, Rotation, Reset, HlrOff, HlrOn, Shading, Wireframe };
 enum class RaytraceAction { ToolRaytracing, ToolShadows, ToolReflections, ToolAntialiasing };
