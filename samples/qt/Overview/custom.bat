e814383df170699bade0f3f8a85e3c60ff1d07b2
asuraven <asuraven@opencascade.com>
2020-09-24 10:40:06 +0300

    fix

--- samples/qt/Overview/custom.bat
+++ samples/qt/Overview/custom.bat
@@ -2,5 +2,5 @@
 rem Define QTDIR variable - path to QT directory
 rem and CASROOT variable - path to CasCade installation directory
 
-set CASROOT=C:\OpenCASCADE-7.5.0-vc14-64\opencascade-7.5.0
-set QTDIR=C:\Work\3d_party\qt5.11.2-vc14-64
+set CASROOT=
+set QTDIR=
